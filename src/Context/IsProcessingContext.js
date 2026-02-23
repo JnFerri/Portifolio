3e81a99031dc7c0ef38408a622bd5a637f24ce71
Jean Ferri <jeanferrimh@gmail.com>
2026-02-22 22:49:47 -0300

    create: Context folder and context to isProcessing to indentify how one function is running anddenied double activate

---
+++ src/Context/IsProcessingContext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from "react";
+
+const IsProcessingContext = createContext()
+
+const IsProcessingProvider = ({children}) => {
+    const [IsProcessing, setIsProcessing] = useState(false)
+    return (
+        <IsProcessingContext.Provider value={[IsProcessing , setIsProcessing]}>
+            {children}
+            </IsProcessingContext.Provider>
+    )
+}
+
+const useIsProcessing = () => {return useContext(IsProcessingContext)}
+
+export {useIsProcessing , IsProcessingProvider}
